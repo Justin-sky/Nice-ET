d29b3cbb512a0a904069b1a936f075db26319cf8
Justin-sky <284139554@qq.com>
2020-08-07 17:56:41 +0800

    拆分类

---
+++ Model/Model/Config/Component/ConfigComponentHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NiceET
+{
+    public static class ConfigComponentHelper
+	{
+		public static void Awake(this ConfigComponent self)
+		{
+			self.Load();
+		}
+
+		public static void Load(this ConfigComponent self)
+		{
+			self.AllConfig.Clear();
+			HashSet<Type> types = Game.EventSystem.GetTypes(typeof(ConfigAttribute));
+
+			foreach (Type type in types)
+			{
+				object obj = Activator.CreateInstance(type);
+
+				ACategory iCategory = obj as ACategory;
+				if (iCategory == null)
+				{
+					throw new Exception($"class: {type.Name} not inherit from ACategory");
+				}
+				iCategory.BeginInit();
+				iCategory.EndInit();
+
+				self.AllConfig[iCategory.ConfigType] = iCategory;
+			}
+		}
+	}
+}
--- Model/Model/Config/System/ConfigComponentSystem.cs
+++ Model/Model/Config/System/ConfigComponentSystem.cs
@@ -29,32 +29,5 @@ namespace NiceET
 		}
 	}
 
-	public static class ConfigComponentSystem
-	{
-		public static void Awake(this ConfigComponent self)
-		{
-			self.Load();
-		}
-
-		public static void Load(this ConfigComponent self)
-		{
-			self.AllConfig.Clear();
-			HashSet<Type> types = Game.EventSystem.GetTypes(typeof(ConfigAttribute));
-
-			foreach (Type type in types)
-			{
-				object obj = Activator.CreateInstance(type);
-
-				ACategory iCategory = obj as ACategory;
-				if (iCategory == null)
-				{
-					throw new Exception($"class: {type.Name} not inherit from ACategory");
-				}
-				iCategory.BeginInit();
-				iCategory.EndInit();
-
-				self.AllConfig[iCategory.ConfigType] = iCategory;
-			}
-		}
-	}
+	
 }
